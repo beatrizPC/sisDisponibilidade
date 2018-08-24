9c32725eb66a649babe66cfb3c9f2494956bc3b1
VitoriaAdame <vitoriamoura423@gmail.com>
2018-08-24 09:03:00 -0300

    Add files via upload

---
+++ msg.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SisDisponibilidade - Aviso</title>
+</head>
+<body>
+	<p><?php echo $_GET['msg']."</br>"; ?></p>
+	<a href = "javascript:history.back()"> Voltar </a>
+	<a href="/sisdisponibilidade/view/index.html"> Menu</a>
+</body>
+</html>
